651330e603cd6a9912d5b166a73f6e3f5a3d57af
Jolle-Jay <j.lopezzz@hotmail.com>
2025-10-30 19:07:16 +0100

    made user class

---
+++ User.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace app;
+
+public class User
+{
+    public string Email;
+
+    string _password;
+
+    public User(string username, string password)
+    {
+        Email = username;
+        _password = password;
+    }
+
+    public bool TryLogin(string username, string password)
+    {
+        return username == Email && password == _password;
+    }
+}
